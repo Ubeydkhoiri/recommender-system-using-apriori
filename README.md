c098a36c96b75af7ae0376cbeb19f16aa92b6b8e
Ubeyd Khoiri <Ubeydkhoiri@gmail.com>
2024-05-21 22:13:50 +0700

    edit app.py

--- README.md
+++ README.md
@@ -1,27 +1,46 @@
 # Recommendation System using Apriori Algorithm
 
-This project implements a recommendation system using the Apriori algorithm for frequent itemset mining and association rule generation.
+This project implements a recommendation system using both the Apriori algorithm for frequent itemset mining and association rule generation, as well as collaborative filtering for personalized recommendations.
 
 ## Introduction
 
-The recommendation system aims to suggest relevant products to users based on their past purchase history. It utilizes the Apriori algorithm to discover frequent itemsets from transaction data and generate association rules to make recommendations.
+The goal of this recommendation system is to suggest relevant products to users based on their past purchase history. It utilizes the Apriori algorithm to discover frequent itemsets from transaction data and generate association rules, alongside collaborative filtering techniques to enhance recommendation accuracy.
 
 ## Installation
+
+To set up the project, follow these steps:
 
 1. Clone the repository:
 
     ```bash
-    git clone https://github.com/Ubeydkhoiri/recommendation-product.git
+    git clone https://github.com/Ubeydkhoiri/recommender-system-using-apriori.git
+
+    # move to the directory
+    cd recommender-system-using-apriori
     ```
 
-2. Install dependencies:
+2. Create your virtual environtment:
+
+    ```bash
+    python -m venv venv_name
+
+    # activate your virtual envirotnment
+    venv_name\Scripts\activate
+    ```
+
+3. Install dependencies:
 
     ```bash
     pip install -r requirements.txt
     ```
 
-3. Run the recommendation system:
+4. Run the recommendation system:
 
     ```bash
     streamlit run app.py
     ```
+
+## Result Example
+Below is an example of the recommendation system's output after running `app.py`:
+
+![Result Example](result-example.png)
--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import streamlit as st
 import pandas as pd
 from pathlib import Path
 
+# Define the parent directory path
 PARENT_PATH = Path(__file__).resolve().parent
 
 
@@ -12,14 +13,16 @@ product_details_df['Details'] = product_details_df['Product ID'].astype(str) + '
                                 +  product_details_df['Category'] + ' ' \
                                 '$' + product_details_df['Price'].astype(str) + ')'
 
+# Load association rules
 rules = joblib.load('rules.joblib')
 
 def fetch_image(product_id):
+    """Fetches the image path for a given product ID."""
     image_path = f"images/{product_id}.png"
     return image_path
 
-# Function to recommend products based on a given product
 def recommend_products(product_id, num_recommendations=10):
+    """Recommends products based on a given product ID."""
     related_products = rules[(rules['antecedents'] == frozenset({product_id}))]
     related_products_sorted = related_products.sort_values(by='lift', ascending=False)
     related_products_sorted = related_products_sorted['consequents'].apply(lambda x: list(x))
@@ -34,6 +37,7 @@ def recommend_products(product_id, num_recommendations=10):
     return recommended_products
 
 def recommend(product_id):
+    """Generates recommendations for a given product ID."""
     recommended_products = recommend_products(product_id)
     product_names = []
     product_images = []
@@ -46,34 +50,49 @@ def recommend(product_id):
     return product_names, product_images
 
 
-st.header('Product Recommender System')
+def main():
+    """Main function to run the Streamlit app."""
+    st.header('Product Recommender System')
 
-product_list = product_details_df['Details'].values
-selected_movie = st.selectbox(
-    "Type or select a movie from the dropdown",
-    product_list
-)
+    product_list = product_details_df['Details'].values
+    selected_product = st.selectbox(
+        "Type or select a movie from the dropdown",
+        product_list
+    )
 
-if st.button('Show Recommendation'):
-    recommended_product_names,recommended_product_images = recommend(int(selected_movie.split(' ')[0]))
-    col1, col2, col3 = st.columns(3)
-    with col1:
-        st.text(recommended_product_names[0])
-        st.image(recommended_product_images[0])
-    with col2:
-        st.text(recommended_product_names[1])
-        st.image(recommended_product_images[1])
-    with col3:
-        st.text(recommended_product_names[2])
-        st.image(recommended_product_images[2])
+    if st.button('Show Recommendation'):
+        selected_product_id = int(selected_product.split(' ')[0])
+        recommended_product_names, recommended_product_images = recommend(selected_product_id)
 
-    col4, col5, col6 = st.columns(3)
-    with col4:
-        st.text(recommended_product_names[3])
-        st.image(recommended_product_images[3])
-    with col5:
-        st.text(recommended_product_names[4])
-        st.image(recommended_product_images[4])
-    with col6:
-        st.text(recommended_product_names[5])
-        st.image(recommended_product_images[5])
+        # Display text explaining user choice
+        st.write(f"You choose this product")
+         # Display example result image
+        st.image(fetch_image(selected_product_id), caption=f"Product ID {selected_product_id}", use_column_width=True)
+
+        # Display text explaining user choice
+        st.write(f"You may like these product")
+        # Display recommended products
+        col1, col2, col3 = st.columns(3)
+        with col1:
+            st.text(recommended_product_names[0])
+            st.image(recommended_product_images[0])
+        with col2:
+            st.text(recommended_product_names[1])
+            st.image(recommended_product_images[1])
+        with col3:
+            st.text(recommended_product_names[2])
+            st.image(recommended_product_images[2])
+
+        col4, col5, col6 = st.columns(3)
+        with col4:
+            st.text(recommended_product_names[3])
+            st.image(recommended_product_images[3])
+        with col5:
+            st.text(recommended_product_names[4])
+            st.image(recommended_product_images[4])
+        with col6:
+            st.text(recommended_product_names[5])
+            st.image(recommended_product_images[5])
+
+if __name__ == "__main__":
+    main()
